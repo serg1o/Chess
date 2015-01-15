2f6634743fa7551ec7e19fefad7c8a79e4f699d5
Zachary Perlmutter <zrp200@gmail.com>
2015-01-15 15:45:31 -0800

    Moved Player to Chess module

---
+++ lib/chess/player.rb
@@ -0,0 +1,10 @@
+module Chess
+  class Player
+    attr_reader :color, :order_player, :computer_player
+    attr_accessor :queen_rook_moved, :king_rook_moved, :king_moved
+    def initialize(color, order)
+      @order_player = order #player1, player2
+      @color = color
+    end
+  end
+end
