86b7a8bcb0ea7c77b3b38377ffdc8bd5172a2ed1
Zachary Perlmutter <zrp200@gmail.com>
2015-01-14 21:44:28 -0800

    Moved Pawn to Chess module

--- lib/pawn.rb
+++
@@ -1,9 +0,0 @@
-require "piece"
-class Pawn < Piece
-  attr_accessor :hypo_moves, :enpassant_move
-  def initialize(color)
-    super color
-    @hypo_moves = color == BLACK ? [[0, 1], [0, 2], [1, 1], [-1, 1]] : [[0, -1], [0, -2], [-1, -1], [1, -1]]
-    @enpassant_move = Array.new
-  end
-end
