0ee3f3ce801dbea901fa18340d3f6905cfa7c4e0
Zachary Perlmutter <zrp200@gmail.com>
2015-01-16 10:26:04 -0800

    Update pawn.rb

--- lib/chess/pawn.rb
+++ lib/chess/pawn.rb
@@ -3,7 +3,7 @@ module Chess
   class Pawn < Piece
     attr_accessor :hypo_moves, :enpassant_move
     def initialize(color)
-      super color
+      super
       @hypo_moves = color == BLACK ? [[0, 1], [0, 2], [1, 1], [-1, 1]] : [[0, -1], [0, -2], [-1, -1], [1, -1]]
       @enpassant_move = Array.new
     end
